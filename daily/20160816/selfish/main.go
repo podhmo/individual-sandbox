20091d92fb3200fa3fa80662ae6231fdacfc3cba
podhmo <ababjam61+github@gmail.com>
2016-08-16 11:24:25 +0900

    tidily

--- daily/20160816/selfish/main.go
+++ daily/20160816/selfish/main.go
@@ -21,25 +21,32 @@ func ppJSON(target interface{}) {
 	out.WriteTo(os.Stdout)
 }
 
+// CreateClient is factory of github client
+func CreateClient(token string) *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	return github.NewClient(tc)
+}
+
+// AppMain is main function of Application
+func AppMain(client *github.Client) {
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List("", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	ppJSON(repos)
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		fmt.Fprintf(os.Stderr, "selfish <token>\n")
 		os.Exit(1)
 	}
 	token := os.Args[1]
-
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
-
-	client := github.NewClient(tc)
-
-	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List("", nil)
-
-	if err != nil {
-		panic(err)
-	}
-    ppJSON(repos)
+	client := CreateClient(token)
+	AppMain(client)
 }
