2a915451d46a485b9565224ebcdcef319322ca44
podhmo <ababjam61+github@gmail.com>
2016-08-19 15:26:05 +0900

    gofmt

--- daily/20160819/example_stringio/printfirst_test.go
+++ daily/20160819/example_stringio/printfirst_test.go
@@ -3,18 +3,18 @@ package main
 import (
 	"bytes"
 	"io"
-	"testing"
 	"strings"
+	"testing"
 )
 
 func TestPrintFirst(t *testing.T) {
 	var r io.Reader
 	r = bytes.NewBufferString("hello.\nthis is the first message for doing something new.")
-    buf := &bytes.Buffer{}
+	buf := &bytes.Buffer{}
 	printFirst(r, buf)
 
-    output := buf.String()
-    if !strings.Contains(output, "hello") {
-        t.Errorf("%q must contains %q", output, "hello")
-    }
+	output := buf.String()
+	if !strings.Contains(output, "hello") {
+		t.Errorf("%q must contains %q", output, "hello")
+	}
 }
