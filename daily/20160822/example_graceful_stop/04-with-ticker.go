7aceb7ad0e20b58570f4669e9f4e89cb0ab74421
podhmo <ababjam61+github@gmail.com>
2016-08-22 23:09:53 +0900

    with ticker

---
+++ daily/20160822/example_graceful_stop/04-with-ticker.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func calc(id int, n int) {
+	st := time.Now()
+	fmt.Printf("start(%2d): cost=%d\n", id, n)
+	time.Sleep(time.Duration(n) * time.Millisecond)
+	fmt.Printf(" end(%2d): cost=%d, time=%s\n", id, n, time.Now().Sub(st))
+}
+
+func runTicker(d time.Duration, st time.Time) func() {
+	fmt.Println("****tick: start")
+	ticker := time.NewTicker(time.Duration(100) * time.Millisecond)
+
+	stop := make(chan interface{})
+	go func() {
+	loop:
+		for {
+			select {
+			case t := <-ticker.C:
+				fmt.Printf("****tick: %s\n", t.Sub(st))
+			case <-stop:
+				break loop
+			}
+		}
+	}()
+
+	return func() {
+		fmt.Println("****tick: stop")
+		ticker.Stop()
+		close(stop)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	st := time.Now()
+	tickerCloseFn := runTicker(time.Duration(100)*time.Millisecond, st)
+
+	for i := 0; i < 20; i++ {
+		i := i
+		wg.Add(1)
+		go func() {
+			calc(i, rand.Intn(2000))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	tickerCloseFn()
+	fmt.Printf("end with %s\n", time.Now().Sub(st))
+}
