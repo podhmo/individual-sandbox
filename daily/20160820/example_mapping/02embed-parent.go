384cb4e13c5df51300353200cfb53ce28fc38fac
podhmo <ababjam61+github@gmail.com>
2016-08-22 23:20:35 +0900

    forget to commit changes

--- daily/20160820/example_mapping/02embed-parent.go
+++ daily/20160820/example_mapping/02embed-parent.go
@@ -14,6 +14,10 @@ type Account struct {
 	IsActive bool
 }
 
+type Node struct {
+	Account Account
+}
+
 type Maskable interface {
 	Mask() interface{}
 }
@@ -23,19 +27,18 @@ func (u User) Mask() interface{} {
 	return u
 }
 
+func (n Node) Mask() interface{} {
+	// panic
+	n.Account = n.Account.Mask().(Account)
+	return n
+}
+
 func main() {
 	user := User{Name: "foo", Password: ":this is secret password:"}
 	account := Account{User: user, IsActive: true}
 
 	{
-		var target Maskable = user
-		fmt.Println("----------------------------------------")
-		fmt.Printf("\tbefore: %#v\n", target)
-		fmt.Printf("\tmapped: %#v\n", target.Mask())
-		fmt.Printf("\tafter : %#v\n", target)
-	}
-	{
-		var target Maskable = account
+		var target Maskable = &Node{Account: account}
 		fmt.Println("----------------------------------------")
 		fmt.Printf("\tbefore: %#v\n", target)
 		// we need `mapped: main.Account{main.User{Name:"foo", Password:"***masked***"} IsActive:true}` but..
