9d9d8e41e13c93ff0250ab63f4d526431537bc21
podhmo <ababjam61+github@gmail.com>
2016-08-24 00:47:00 +0900

    gofmt

--- daily/20160823/example_wait/01-with-waitgroup.go
+++ daily/20160823/example_wait/01-with-waitgroup.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
-	"time"
-	"fmt"
 	"sync"
+	"time"
 )
 
 func task(id int, d time.Duration) error {
@@ -16,21 +16,21 @@ func task(id int, d time.Duration) error {
 }
 
 func main() {
-    st := time.Now()
-    var wg sync.WaitGroup
+	st := time.Now()
+	var wg sync.WaitGroup
 
 	for i := 0; i < 100; i++ {
 		i := i
-        wg.Add(1)
+		wg.Add(1)
 		go func() {
 			err := task(i, time.Duration(rand.Intn(1000))*time.Millisecond)
-            if err != nil {
-                log.Printf("err: %s\n", err)
-            }
-            wg.Done()
+			if err != nil {
+				log.Printf("err: %s\n", err)
+			}
+			wg.Done()
 		}()
 	}
 
-    wg.Wait()
-    fmt.Printf("end with %s\n", time.Now().Sub(st))
+	wg.Wait()
+	fmt.Printf("end with %s\n", time.Now().Sub(st))
 }
