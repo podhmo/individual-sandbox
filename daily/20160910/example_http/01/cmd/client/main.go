4269c8a0b4468c5ff8d8be2e4392e004565db8e0
podhmo <ababjam61+github@gmail.com>
2016-09-11 00:43:41 +0900

    add test

--- daily/20160910/example_http/01/cmd/client/main.go
+++ daily/20160910/example_http/01/cmd/client/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"fmt"
 )
 
 func main() {
 	client := &http.Client{}
-    url := fmt.Sprintf("http://localhost:8080/%s", os.Args[1])
+	url := fmt.Sprintf("http://localhost:8080/%s", os.Args[1])
 	response, err := client.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
-    b, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Println(string(b))
 }
