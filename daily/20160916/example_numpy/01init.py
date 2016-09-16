f7e8b3ecb366f48261e1b125b27f764f6296ae99
podhmo <ababjam61+github@gmail.com>
2016-09-16 16:45:00 +0900

    np

---
+++ daily/20160916/example_numpy/01init.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+a = np.array([0, 1, 2, 3, 4])
+print(a)
+b = np.zeros(5)
+print(b)
+c = np.ones(5)
+print(c)
+d = np.arange(5)
+print(d)
+
+# [0 1 2 3 4]
+# [ 0.  0.  0.  0.  0.]
+# [ 1.  1.  1.  1.  1.]
+# [0 1 2 3 4]
