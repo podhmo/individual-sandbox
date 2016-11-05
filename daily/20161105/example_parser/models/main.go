e8e7245b510d0fa6189cdca7de3ae6e51dfe2440
podhmo <ababjam61+github@gmail.com>
2016-11-06 02:49:51 +0900

    hmm

--- daily/20161105/example_parser/models/main.go
+++ daily/20161105/example_parser/models/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"go/parser"
 	"go/token"
 	"log"
+	"path"
 
 	"flag"
 
-	"github.com/davecgh/go-spew/spew"
+	"encoding/json"
+	"strconv"
 )
 
 var target *string
@@ -17,7 +20,7 @@ func init() {
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 	log.Println(*target)
 	if *target == "" {
 		log.Fatal("app -target <file>")
@@ -25,11 +28,38 @@ func main() {
 	run(*target)
 }
 
-func run(fname string) {
+func scanImportsRec(m map[string]string, fname string) error {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, fname, nil, 0)
 	if err != nil {
+		return err
+	}
+	for _, spec := range f.Imports {
+		pathvalue, err := strconv.Unquote(spec.Path.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		name := path.Base(pathvalue)
+		if spec.Name != nil {
+			name = spec.Name.Name
+		}
+		_, exists := m[name]
+		if !exists {
+			m[name] = pathvalue
+		}
+	}
+	return nil
+}
+
+func run(fname string) {
+	m := make(map[string]string)
+	err := scanImportsRec(m, fname)
+	if err != nil {
 		log.Fatal(err)
 	}
-	spew.Dump(f)
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
 }
