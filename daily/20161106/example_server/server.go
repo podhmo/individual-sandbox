5ef40334ff62c3e68cfd94289ecec98cb2063afa
podhmo <ababjam61+github@gmail.com>
2016-11-06 21:52:10 +0900

    postform, postjson

--- daily/20161106/example_server/server.go
+++ daily/20161106/example_server/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -10,10 +12,45 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world")
 }
 
+// Echo :
+func Echo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(404)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%s", err)
+	}
+	fmt.Fprintf(w, "%s", r.Form.Get("message"))
+}
+
+// Echo2 :
+func Echo2(w http.ResponseWriter, r *http.Request) {
+	reader := io.LimitReader(r.Body, 1024*1024)
+	decoder := json.NewDecoder(reader)
+	type B struct {
+		Message string `json:"message"`
+	}
+	var b B
+	if err := decoder.Decode(&b); err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "%s", err)
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	// do something
+	if err := json.NewEncoder(w).Encode(&b); err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "%s", err)
+	}
+}
+
 // NewHandler :
 func NewHandler() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Hello)
+	mux.HandleFunc("/echo", Echo)
+	mux.HandleFunc("/echo2", Echo2)
 	return mux
 }
 
--- daily/20161106/example_server/server_test.go
+++ daily/20161106/example_server/server_test.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
 	mux := NewHandler()
 	ts := httptest.NewServer(mux)
+	defer ts.Close()
 	response, err := http.Get(ts.URL)
 	if err != nil {
 		t.Fatal(err)
@@ -21,6 +26,49 @@ func TestHello(t *testing.T) {
 	}
 	if string(b) != "hello world" {
 		t.Errorf("body: expected=%q, actual=%q", "hello world", string(b))
+	}
+}
+
+func TestEcho(t *testing.T) {
+	mux := NewHandler()
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+	response, err := http.PostForm(ts.URL+"/echo", url.Values{"message": {"hello"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b) != "hello" {
+		t.Errorf("body: expected=%q, actual=%q", "hello", string(b))
+	}
+}
+
+func TestEcho2(t *testing.T) {
+	mux := NewHandler()
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+	payload, err := json.Marshal(struct {
+		Message string `json:"message"`
+	}{Message: "hello"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	response, err := http.Post(ts.URL+"/echo2", "application/json", bytes.NewBuffer(payload))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if strings.TrimRight(string(b), "\n") != strings.TrimRight(string(payload), "\n") {
+		t.Errorf("body: expected=%q, actual=%q", string(payload), string(b))
 	}
 }
 
@@ -38,5 +86,50 @@ func TestHelloHandler(t *testing.T) {
 	}
 	if string(b) != "hello world" {
 		t.Errorf("body: expected=%q, actual=%q", "hello world", string(b))
+	}
+}
+
+func TestEchoHandler(t *testing.T) {
+	r := strings.NewReader(url.Values{"message": {"hello"}}.Encode())
+	req := httptest.NewRequest("POST", "http://localhost:8080/echo", r)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	targetAPI := Echo
+
+	targetAPI(w, req)
+
+	b, err := ioutil.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b) != "hello" {
+		t.Errorf("body: expected=%q, actual=%q", "hello", string(b))
+	}
+}
+
+func TestEcho2Handler(t *testing.T) {
+	type B struct {
+		Message string `json:"message"`
+	}
+	var buf bytes.Buffer
+	body := &B{Message: "hello"}
+	if err := json.NewEncoder(&buf).Encode(&body); err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest("POST", "http://localhost:8080/echo2", &buf)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	targetAPI := Echo2
+
+	targetAPI(w, req)
+	var body2 B
+	if err := json.NewDecoder(w.Body).Decode(&body2); err != nil {
+		t.Fatal(err)
+	}
+	if body2.Message != "hello" {
+		t.Errorf("body: expected=%q, actual=%q", "hello", body2.Message)
 	}
 }
