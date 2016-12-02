c5318945491cf71e2b6a022b4d0b050378957f5a
podhmo <ababjam61+github@gmail.com>
2016-12-03 03:06:54 +0900

    hmm

--- daily/20161201/example_prestring/output2/github.go
+++ daily/20161201/example_prestring/output2/github.go
@@ -1,6 +1,7 @@
 package autogen
 
 import (
+	"github.com/go-openapi/strfmt"
 	"time"
 )
 
@@ -15,22 +16,22 @@ Github
     Source
 */
 type Github struct {
-	CloneURL         string       `json:"clone_url"`
+	CloneURL         strfmt.Uri   `json:"clone_url"`
 	CreatedAt        time.Time    `json:"created_at"`
 	DefaultBranch    string       `json:"default_branch"`
 	Description      string       `json:"description"`
 	Fork             int          `json:"fork"`
 	ForksCount       int          `json:"forks_count"`
 	FullName         string       `json:"full_name"`
-	GitURL           string       `json:"git_url"`
-	HTMLURL          string       `json:"html_url"`
+	GitURL           strfmt.Uri   `json:"git_url"`
+	HTMLURL          strfmt.Uri   `json:"html_url"`
 	HasDownloads     int          `json:"has_downloads"`
 	HasIssues        int          `json:"has_issues"`
 	HasWiki          int          `json:"has_wiki"`
-	Homepage         string       `json:"homepage"`
+	Homepage         strfmt.Uri   `json:"homepage"`
 	ID               int          `json:"id"`
 	Language         interface{}  `json:"language"`
-	MirrorURL        string       `json:"mirror_url"`
+	MirrorURL        strfmt.Uri   `json:"mirror_url"`
 	Name             string       `json:"name"`
 	OpenIssuesCount  int          `json:"open_issues_count"`
 	Organization     Organization `json:"organization"`
@@ -44,49 +45,49 @@ type Github struct {
 	Source           Parent       `json:"source"`
 	StargazersCount  int          `json:"stargazers_count"`
 	SubscribersCount int          `json:"subscribers_count"`
-	SvnURL           string       `json:"svn_url"`
-	URL              string       `json:"url"`
+	SvnURL           strfmt.Uri   `json:"svn_url"`
+	URL              strfmt.Uri   `json:"url"`
 	UpdatedAt        time.Time    `json:"updated_at"`
 	WatchersCount    int          `json:"watchers_count"`
 }
 
 type Organization struct {
-	AvatarURL         string `json:"avatar_url"`
-	EventsURL         string `json:"events_url"`
-	FollowersURL      string `json:"followers_url"`
-	FollowingURL      string `json:"following_url"`
-	GistsURL          string `json:"gists_url"`
-	GravatarID        string `json:"gravatar_id"`
-	HTMLURL           string `json:"html_url"`
-	ID                int    `json:"id"`
-	Login             string `json:"login"`
-	OrganizationsURL  string `json:"organizations_url"`
-	ReceivedEventsURL string `json:"received_events_url"`
-	ReposURL          string `json:"repos_url"`
-	SiteAdmin         int    `json:"site_admin"`
-	StarredURL        string `json:"starred_url"`
-	SubscriptionsURL  string `json:"subscriptions_url"`
-	Type              string `json:"type"`
-	URL               string `json:"url"`
+	AvatarURL         strfmt.Uri `json:"avatar_url"`
+	EventsURL         strfmt.Uri `json:"events_url"`
+	FollowersURL      strfmt.Uri `json:"followers_url"`
+	FollowingURL      strfmt.Uri `json:"following_url"`
+	GistsURL          strfmt.Uri `json:"gists_url"`
+	GravatarID        string     `json:"gravatar_id"`
+	HTMLURL           strfmt.Uri `json:"html_url"`
+	ID                int        `json:"id"`
+	Login             string     `json:"login"`
+	OrganizationsURL  strfmt.Uri `json:"organizations_url"`
+	ReceivedEventsURL strfmt.Uri `json:"received_events_url"`
+	ReposURL          strfmt.Uri `json:"repos_url"`
+	SiteAdmin         int        `json:"site_admin"`
+	StarredURL        strfmt.Uri `json:"starred_url"`
+	SubscriptionsURL  strfmt.Uri `json:"subscriptions_url"`
+	Type              string     `json:"type"`
+	URL               strfmt.Uri `json:"url"`
 }
 
 type Parent struct {
-	CloneURL        string       `json:"clone_url"`
+	CloneURL        strfmt.Uri   `json:"clone_url"`
 	CreatedAt       time.Time    `json:"created_at"`
 	DefaultBranch   string       `json:"default_branch"`
 	Description     string       `json:"description"`
 	Fork            int          `json:"fork"`
 	ForksCount      int          `json:"forks_count"`
 	FullName        string       `json:"full_name"`
-	GitURL          string       `json:"git_url"`
-	HTMLURL         string       `json:"html_url"`
+	GitURL          strfmt.Uri   `json:"git_url"`
+	HTMLURL         strfmt.Uri   `json:"html_url"`
 	HasDownloads    int          `json:"has_downloads"`
 	HasIssues       int          `json:"has_issues"`
 	HasWiki         int          `json:"has_wiki"`
-	Homepage        string       `json:"homepage"`
+	Homepage        strfmt.Uri   `json:"homepage"`
 	ID              int          `json:"id"`
 	Language        interface{}  `json:"language"`
-	MirrorURL       string       `json:"mirror_url"`
+	MirrorURL       strfmt.Uri   `json:"mirror_url"`
 	Name            string       `json:"name"`
 	OpenIssuesCount int          `json:"open_issues_count"`
 	Owner           Organization `json:"owner"`
@@ -96,8 +97,8 @@ type Parent struct {
 	SSHURL          string       `json:"ssh_url"`
 	Size            int          `json:"size"`
 	StargazersCount int          `json:"stargazers_count"`
-	SvnURL          string       `json:"svn_url"`
-	URL             string       `json:"url"`
+	SvnURL          strfmt.Uri   `json:"svn_url"`
+	URL             strfmt.Uri   `json:"url"`
 	UpdatedAt       time.Time    `json:"updated_at"`
 	WatchersCount   int          `json:"watchers_count"`
 }
