27e18e3055033f6d0871a1527e71cea93a08420b
podhmo <ababjam61+github@gmail.com>
2017-05-13 14:06:08 +0900

    hmm

---
+++ daily/20170513/example_jinja2/resources/00minij2cli.py
@@ -0,0 +1,34 @@
+import jinja2
+import sys
+from dictknife import loading, deepmerge
+
+
+def run(template_path, data):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(["."]), undefined=jinja2.StrictUndefined
+    )
+    t = env.get_or_select_template(template_path)
+    return t.render(**data)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", action="append", help="support yaml, json, toml", default=[])
+    parser.add_argument("template")
+    parser.add_argument("--input-format", default=None)
+    parser.add_argument("--output-format", default="raw")
+    parser.add_argument("--dst", default=None)
+
+    args = parser.parse_args()
+    loading.setup()
+
+    data = deepmerge(*[loading.loadfile(d) for d in args.data], override=True)
+    if args.input_format is not None:
+        data = deepmerge(data, loading.load(sys.stdin, format=args.input_format), override=True)
+    result = run(args.template, data)
+    loading.dumpfile(result, args.dst, format=args.output_format)
+
+
+if __name__ == "__main__":
+    main()
