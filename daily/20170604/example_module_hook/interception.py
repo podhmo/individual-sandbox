a051d776f4773bbd96c5e3efeb5d4b3cd4742015
podhmo <ababjam61+github@gmail.com>
2017-06-04 22:25:12 +0900

    -

--- daily/20170604/example_module_hook/interception.py
+++ daily/20170604/example_module_hook/interception.py
@@ -1,18 +1,24 @@
 import sys
+import inspect
+import traceback
 
 
-def hookwrap(fn):
+def hookwrap(fn, verbose):
     if getattr(fn, "wrapped", False):
         return fn
 
-    def wrap(*args, **kwargs):
-        print("@@", args, kwargs, file=sys.stderr)
-        return fn(*args, **kwargs)
+    def wrap(filename):
+        print("load", filename, file=sys.stderr)
+        frame = inspect.currentframe().f_back
+        while "importlib" in frame.f_code.co_filename:
+            frame = frame.f_back
+        traceback.print_stack(frame, limit=1, file=sys.stderr)
+        return fn(filename)
 
     wrap.wrapped = True
     return wrap
 
 
-def setup():
+def setup(verbose=False):
     for i in range(len(sys.path_hooks)):
-        sys.path_hooks[i] = hookwrap(sys.path_hooks[i])
+        sys.path_hooks[i] = hookwrap(sys.path_hooks[i], verbose=verbose)
