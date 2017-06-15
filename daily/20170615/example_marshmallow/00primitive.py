de67c448dbcd81b6e00425f53518aa6adb1abdfb
podhmo <ababjam61+github@gmail.com>
2017-06-15 12:02:32 +0900

    -

---
+++ daily/20170615/example_marshmallow/00primitive.py
@@ -0,0 +1,43 @@
+from marshmallow import Schema, fields
+from marshmallow import MarshalResult
+
+
+class PrimitiveValueSchema:
+    schema_class = None
+    key = "value"
+    missing = None
+
+    def __init__(self, *args, **kwargs):
+        self.schema = self.__class__.schema_class(*args, **kwargs)
+
+    def load(self, value):  # don't support many
+        data = {self.key: value}
+        return self.schema.load(data)
+
+    def dump(self, value):  # don't support many
+        data = {self.key: value}
+        r, errors = self.schema.dump(data, update_fields=False)
+        return MarshalResult(
+            data=r.get(self.key) or self.missing,
+            errors=errors.get(self.key) or errors,
+        )
+
+
+class IntSchema(PrimitiveValueSchema):
+    class schema_class(Schema):
+        value = fields.Integer(required=True)
+
+
+class IntListSchema(PrimitiveValueSchema):
+    key = "values"
+
+    class schema_class(Schema):
+        values = fields.List(fields.Integer(), required=True)
+
+
+print(IntSchema().load(10))
+print(IntSchema().dump(10))
+print(IntSchema().load("foo"))
+print(IntSchema().dump("foo"))
+print(IntListSchema().load([10, 20]))
+print(IntListSchema().dump(["foo", "bar"]))
