c8075f3ff517ea46728cdc7077ba8bea2e89448d
podhmo <ababjam61+github@gmail.com>
2017-06-25 22:06:03 +0900

    mongomock

---
+++ daily/20170625/example_mongo/00hello.py
@@ -0,0 +1,60 @@
+import mongomock
+
+# see also: https://github.com/mongomock/mongomock/blob/develop/tests/test__bulk_operations.py
+
+
+def get_client():
+    return mongomock.MongoClient()
+
+
+client = get_client()
+db = client["me"]
+
+db.people.drop()
+db.people.create_index("gender", unique=False, name="idx_gender", sparse=True, background=True)
+
+print("pre insert count: ", db.people.count())
+db.people.insert_one({"name": "foo", "age": 20, "gender": "F"})
+db.people.insert_one({"name": "bar", "age": 20, "gender": "F"})
+db.people.insert_one({"name": "boo", "age": 21, "gender": "M"})
+print("post insert count: ", db.people.count())
+
+print("query")
+for row in db.people.find({"gender": "F"}):
+    print("\t", row)
+
+print("update")
+print("\t", db.people.update_one({"gender": "F"}, {"$set": {"_updated": True}, "$inc": {"age": 1}}))
+print(
+    "\t", db.people.update_many({
+        "gender": "F"
+    }, {"$set": {
+        "_updated": True
+    },
+        "$inc": {
+            "age": 1
+        }})
+)
+
+print("query")
+for row in db.people.find({"gender": "F"}):
+    print("\t", row)
+
+print("pre bulk insert count: ", db.people.count())
+db.people.insert_many(
+    [
+        {
+            "name": "X",
+            "age": 0
+        },
+        {
+            "name": "Y",
+            "age": 0
+        },
+        {
+            "name": "Z",
+            "age": 0
+        },
+    ]
+)
+print("post bulk insert count: ", db.people.count())
