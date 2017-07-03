359d3d745ed9382caae81a2bf542ed07f41b75c9
podhmo <ababjam61+github@gmail.com>
2017-07-03 15:44:12 +0900

    -

---
+++ daily/20170703/example_inflector/00inflection.py
@@ -0,0 +1,14 @@
+import inflection as i
+
+text = "foo_bar_boo"
+print("camelize", i.camelize(text))
+
+print("dasherize", i.dasherize(text))
+
+print("humanize", i.humanize(text))
+
+print("----------------------------------------")
+
+print("pluralize", i.pluralize("dog"))
+
+print("singularize", i.singularize("dogs"))
