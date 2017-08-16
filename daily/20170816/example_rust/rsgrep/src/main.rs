dbb2f08257e1d35efefdffabbb230f552ac78efe
podhmo <ababjam61+github@gmail.com>
2017-08-16 18:17:45 +0900

    -

--- daily/20170816/example_rust/rsgrep/src/main.rs
+++ daily/20170816/example_rust/rsgrep/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::env;
+
+fn usage(){
+    println!("rsgrep <pattern> <filename>")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = match env::args().nth(2) {
+        Some(filename) => filename,
+        None => {
+            usage();
+            return;
+        }
+    };
+
+    let file = match File::open(&filename) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("file {}:{}", filename, e);
+            return;
+        }
+    };
+
+    let input = BufReader::new(file);
+    for line in input.lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => {
+                println!("line {}:{}", filename, e);
+                return;
+            }
+        };
+        println!("{}", line)
+    }
 }
