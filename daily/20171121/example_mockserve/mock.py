899788ff195864ba7ea083b76619a551e81d1dce
podhmo <ababjam61+github@gmail.com>
2017-11-21 16:39:37 +0900

    reload

--- daily/20171121/example_mockserve/mock.py
+++ daily/20171121/example_mockserve/mock.py
@@ -1,18 +1,27 @@
 import json
 from wsgiref.simple_server import make_server
+from importlib import import_module, reload
 
 
-def make_app(filename, transform):
+def make_app(filename, transform, usereload):
     def app(environ, start_response):
         status = '200 OK'
         headers = [('Content-type', 'application/json; charset=utf-8')]
         start_response(status, headers)
         with open(filename) as rf:
             d = json.load(rf)
+
         if transform is not None:
-            transformed = transform(d)
+            modname, fnname = transform.rsplit(":", 1)
+            m = import_module(modname)
+            if usereload:
+                m = reload(m)
+            fn = getattr(m, fnname)
+
+            transformed = fn(d)
             if transformed is not None:
                 d = transformed
+
         return [json.dumps(d).encode("utf-8")]
 
     return app
@@ -23,14 +32,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", required=True, type=int)
     parser.add_argument("--transform", default=None)
+    parser.add_argument("--reload", action="store_true")
     parser.add_argument("filename")
     args = parser.parse_args()
 
-    if args.transform is not None:
-        from importlib import import_module
-        modname, fnname = args.transform.rsplit(":", 1)
-        args.transform = getattr(import_module(modname), fnname)
-    httpd = make_server('', args.port, make_app(args.filename, args.transform))
+    httpd = make_server('', args.port, make_app(args.filename, args.transform, args.reload))
     print("Serving on port {}...".format(args.port))
     # Serve until process is killed
     httpd.serve_forever()
