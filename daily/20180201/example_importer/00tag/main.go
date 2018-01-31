829cbcc8738820ac845c4fafd5d869deb46bc7f8
podhmo <ababjam61+github@gmail.com>
2018-02-01 08:38:14 +0900

    -

---
+++ daily/20180201/example_importer/00tag/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"log"
+	"reflect"
+)
+
+func main() {
+	fset := token.NewFileSet()
+
+	code := (`
+package p
+type S struct {
+	Name string ` + "`" + `json:"name"` + "`" + `
+	Age int ` + "`" + `json:"age"` + "`" + `
+	i  int
+}
+`)
+	conf := types.Config{
+		Importer: importer.Default(),
+		Error: func(err error) {
+			fmt.Printf("!!! %#v\n", err)
+		},
+	}
+
+	file, err := parser.ParseFile(fset, "p", code, parser.AllErrors)
+	pkg, err := conf.Check("p", fset, []*ast.File{file}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	S := pkg.Scope().Lookup("S")
+	internal := S.Type().Underlying().(*types.Struct)
+
+	for i := 0; i < internal.NumFields(); i++ {
+		jsonname, found := reflect.StructTag(internal.Tag(i)).Lookup("json")
+		field := internal.Field(i)
+		fmt.Printf("%v (exported=%t, jsonname=%s, found=%t)\n", field, field.Exported(), jsonname, found)
+	}
+}
