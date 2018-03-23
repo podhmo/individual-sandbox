af2bd0bb427ee7931088c0ae51846ecffdc40b8c
podhmo <ababjam61+github@gmail.com>
2018-03-23 23:58:07 +0900

    -

---
+++ daily/20180323/examplego/02modified/internal/sync_version.go
@@ -0,0 +1,21 @@
+package internal
+
+// SyncExecution :
+func SyncExecution(teams []Team) []Team {
+	var newTeams []Team
+	for _, v := range teams {
+		v.SyncUpdate()
+		newTeams = append(newTeams, v)
+	}
+	return newTeams
+}
+
+// SyncUpdate :
+func (c *Team) SyncUpdate() {
+	var services []Service
+	for _, v := range c.Services {
+		v.Update()
+		services = append(services, v)
+	}
+	c.Services = services
+}
