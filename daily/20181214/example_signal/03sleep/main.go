b90d7a242e167494de0149bfed430f1892d1a150
podhmo <ababjam61+github@gmail.com>
2018-12-14 08:02:17 +0900

    -

--- daily/20181214/example_signal/03sleep/main.go
+++ daily/20181214/example_signal/03sleep/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatalf("%+v", err)
-	}
-}
-
-func run() error {
 	fmt.Println("sleep", 2)
 	time.Sleep(2 * time.Second)
 	fmt.Println("end")
-	return nil
 }
