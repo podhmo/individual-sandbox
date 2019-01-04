5efa348fed3bc49df1a4883f1bfbcebc0becabe5
podhmo <ababjam61+github@gmail.com>
2019-01-04 22:19:10 +0900

    -

--- daily/20190104/example_tmock/user/user_service.go
+++ daily/20190104/example_tmock/user/user_service.go
@@ -1,6 +1,6 @@
 package user
 
-//go:generate mocker --dst user_service_mock.go --pkg mock $GOFILE UserService
+//go:generate mocker --dst mock/user_service_mock.go --pkg mock $GOFILE UserService
 type UserService interface {
 	GetUser(id string) (*User, error)
 }
