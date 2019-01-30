b75eaa1b07e600864e5f8349c3b39b8eaac05d6e
podhmo <ababjam61+github@gmail.com>
2019-01-30 17:35:23 +0900

    -

--- daily/20190130/example_jsonpatch/00unpatch.py
+++ daily/20190130/example_jsonpatch/00unpatch.py
@@ -1,62 +1,89 @@
+import json
 from collections import namedtuple
-cmd = namedtuple("cmd", "op, x, y")
+cmd = namedtuple("cmd", "op, v, v1")
+
+# todo: move
 
 
-def unpatch(src, dst):
-    return list(walk(src, dst))
+def unpatch(src, dst, *, verbose=False):
+    r = Walker().walk(src, dst)
+    rows = merge(r)
 
-
-def walk(src, dst):
-    # xxx: src and dst is None
-    if hasattr(src, "keys"):
-        if not hasattr(dst, "keys"):
-            dst = {"": dst}  # xxx
-        return _walk_dict(src, dst)
-    elif isinstance(src, (list, tuple)):
-        if not isinstance(dst, (list, tuple)):
-            dst = [dst]  # xxx
-        return _walk_list(src, dst)
+    if not verbose:
+        for row in rows:
+            row.pop("from", None)
+            if row["op"] == "remove":
+                row.pop("value", None)
+            yield row
     else:
-        return _walk_atom(src, dst)
+        yield from rows
 
 
-def _walk_dict(src, dst):
-    r = {}
-    for k, v in src.items():
-        if k in dst:
-            r[k] = walk(v, dst[k])
-        else:
-            r[k] = [cmd("remove", x=v, y=None)]
-    for k, v in dst.items():
-        if k in r:
-            continue
-        r[k] = [cmd("add", x=v, y=None)]
-    return list(r.items())
-
-
-def _walk_list(src, dst):
-    r = {}
-    n = min(len(src), len(dst))
-    for i in range(n):
-        r[str(i)] = _walk_list(src[i], dst[i])
-
-    if n == len(src):
-        for i in range(n, len(dst)):
-            r[str(i)] = [cmd(op="add", x=dst[i])]
-    else:
-        for i in range(n, len(src)):
-            r[str(i)] = [cmd(op="add", x=src[i])]
-
-
-def _walk_atom(src, dst):
-    if src is None:
-        return [cmd(op="add", x=dst)]
-    elif dst is None:
-        return [cmd(op="remove", x=src)]
-    elif src != dst:
-        return [cmd(op="replace", x=src, y=dst)]
-    else:
+def merge(r):
+    if r is None:
         return []
+    if not hasattr(r, "keys"):
+        yield {"path": "", "op": r.op, "value": r.v, "from": r.v1}
+    else:
+        for k, v in r.items():
+            for sv in merge(v):
+                yield {
+                    "path": f"/{k}/{sv['path'].lstrip('/')}".rstrip("/"),
+                    "op": sv["op"],
+                    "value": sv["value"],
+                    "from": sv.pop("from", None),
+                }
+
+
+class Walker:
+    def __init__(self):
+        self.move_map = {}  # todo:
+
+    def walk(self, src, dst):
+        # xxx: src and dst is None
+        if hasattr(src, "keys"):
+            return self._walk_dict(src, dst)
+        elif isinstance(src, (list, tuple)):
+            return self._walk_list(src, dst)
+        else:
+            return self._walk_atom(src, dst)
+
+    def _walk_dict(self, src, dst):
+        r = {}
+        for k, v in src.items():
+            if k in dst:
+                r[k] = self.walk(v, dst[k])
+            else:
+                r[k] = cmd("remove", v=v, v1=None)
+        for k, v in dst.items():
+            if k in r:
+                continue
+            r[k] = cmd("add", v=v, v1=None)
+        return r
+
+    def _walk_list(self, src, dst):
+        r = {}
+        n = min(len(src), len(dst))
+        for i in range(n):
+            r[str(i)] = self.walk(src[i], dst[i])
+
+        if n == len(dst):
+            for i in range(n, len(src)):
+                r[str(i)] = cmd(op="remove", v=src[i])
+        else:
+            for i in range(n, len(dst)):
+                r[str(i)] = cmd(op="add", v=dst[i])
+        return r
+
+    def _walk_atom(self, src, dst):
+        if src is None:
+            return cmd(op="add", v=dst)
+        elif dst is None:
+            return cmd(op="remove", v=src)
+        elif src != dst:
+            return cmd(op="replace", v=dst, v1=src)
+        else:
+            return None
 
 
 import unittest  # noqa
@@ -69,27 +96,136 @@ class Tests(unittest.TestCase):
     def test00(self):
         src = {"name": "foo"}
         dst = {"name": "foo"}
-        print(src, dst, "@", self._callFUT(src, dst))
+
+        got = self._callFUT(src, dst)
+        want = []
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
 
     def test01(self):
         src = {"name": "foo"}
         dst = {"name": "bar"}
-        print(src, dst, "@", self._callFUT(src, dst))
+
+        got = self._callFUT(src, dst)
+        want = [{"op": "replace", "path": "/name", "value": "bar"}]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
 
     def test02(self):
         src = {"name": "foo"}
         dst = {}
-        print(src, dst, "@", self._callFUT(src, dst))
+
+        got = self._callFUT(src, dst)
+        want = [{"op": "remove", "path": "/name"}]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
 
     def test03(self):
         src = {}
         dst = {"name": "bar"}
-        print(src, dst, "@", self._callFUT(src, dst))
+
+        got = self._callFUT(src, dst)
+        want = [{"op": "add", "path": "/name", "value": "bar"}]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
 
     def test04(self):
         src = {"point0": {"value": 10}}
         dst = {"point1": {"value": 10}}
-        print(src, dst, "@", self._callFUT(src, dst))
+
+        got = self._callFUT(src, dst)
+        # todo: move
+        want = [
+            {
+                "op": "remove",
+                "path": "/point0",
+            },
+            {
+                "op": "add",
+                "path": "/point1",
+                "value": {
+                    "value": 10
+                },
+            },
+        ]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
+
+    def test05(self):
+        src = {"point0": {"value": 10}}
+        dst = {"point0": {"value": 20}}
+
+        got = self._callFUT(src, dst)
+        want = [
+            {
+                "op": "replace",
+                "path": "/point0/value",
+                "value": 20,
+            },
+        ]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
+
+    def test06(self):
+        src = {"person": {"name": "foo", "age": 20, "type": "P"}}
+        dst = {"person": {"name": "bar", "nickname": "B", "type": "P"}}
+        got = self._callFUT(src, dst)
+        want = [
+            {
+                "op": "replace",
+                "path": "/person/name",
+                "value": "bar",
+            },
+            {
+                "op": "remove",
+                "path": "/person/age",
+            },
+            {
+                "op": "add",
+                "path": "/person/nickname",
+                "value": "B",
+            },
+        ]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
+
+    def test07(self):
+        src = [{}, {"person": {"name": "foo", "age": 20, "type": "P"}}]
+        dst = [{"person": {"name": "bar", "nickname": "B", "type": "P"}}, {}]
+        got = self._callFUT(src, dst)
+        want = [
+            {
+                "path": "/0/person",
+                "op": "add",
+                "value": {
+                    "name": "bar",
+                    "nickname": "B",
+                    "type": "P"
+                }
+            },
+            {
+                "path": "/1/person",
+                "op": "remove",
+            },
+        ]
+        self.assertListEqual(
+            sorted([json.dumps(x, sort_keys=True) for x in got]),
+            sorted([json.dumps(x, sort_keys=True) for x in want])
+        )
 
 
 if __name__ == "__main__":
