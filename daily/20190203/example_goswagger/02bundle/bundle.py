404270981404756a284cd50d8d99b9e5824885a9
podhmo <ababjam61+github@gmail.com>
2019-02-03 17:22:54 +0900

    simplify

--- daily/20190203/example_goswagger/02bundle/bundle.py
+++ daily/20190203/example_goswagger/02bundle/bundle.py
@@ -2,20 +2,17 @@ from handofcats import as_command
 from dictknife import loading
 from dictknife.jsonknife import bundle
 from dictknife import DictWalker
-from dictknife import Accessor
-from dictknife.jsonknife.accessor import access_by_json_pointer
 
 
 @as_command
 def main(*, src: str) -> None:
     precompile_ref_walker = DictWalker(["$precompile-ref"])
-    accessor = Accessor()
 
-    def onload(d, subresolver):
+    def onload(d, resolver):
         for path, sd in precompile_ref_walker.walk(d):
-            sdoc, query = subresolver.resolve(sd.pop("$precompile-ref"))
-            sresolved = access_by_json_pointer(sdoc.doc, query)
-            accessor.assign(d, path[:-1], sresolved)
+            subresolver, query = resolver.resolve(sd.pop("$precompile-ref"))
+            sresolved = subresolver.access(query)
+            subresolver.assign("/".join(path[:-1]), sresolved)
 
     d = bundle(src, onload=onload)
     loading.dumpfile(d)
