873af8a231c78b1154c0cb24e348bb43f9341f38
podhmo <ababjam61+github@gmail.com>
2019-02-03 17:14:26 +0900

    -

--- daily/20190203/example_goswagger/02bundle/bundle.py
+++ daily/20190203/example_goswagger/02bundle/bundle.py
@@ -1,10 +1,9 @@
 from handofcats import as_command
 from dictknife import loading
+from dictknife.jsonknife import bundle
 from dictknife import DictWalker
 from dictknife import Accessor
 from dictknife.jsonknife.accessor import access_by_json_pointer
-from dictknife.jsonknife.resolver import get_resolver_from_filename
-from dictknife.jsonknife import Bundler
 
 
 @as_command
@@ -18,6 +17,5 @@ def main(*, src: str) -> None:
             sresolved = access_by_json_pointer(sdoc.doc, query)
             accessor.assign(d, path[:-1], sresolved)
 
-    resolver = get_resolver_from_filename(src, onload=onload)
-    d = Bundler(resolver).bundle()
+    d = bundle(src, onload=onload)
     loading.dumpfile(d)
