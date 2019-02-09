6d06424c9e9c877741e98f2d6415735963a1cf85
podhmo <ababjam61+github@gmail.com>
2019-02-09 17:01:16 +0900

    -

---
+++ daily/20190208/example_bot/bot/familiar/readme.md
@@ -0,0 +1,11 @@
+setup
+
+```console
+$ pip install slackbot
+```
+
+and creating bot app in slack app page
+
+```
+$ make 00
+```
---
+++ daily/20190208/example_bot/bot/familiar/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name='familiar',
+    version='0.0.0',
+    packages=find_packages(),
+)
