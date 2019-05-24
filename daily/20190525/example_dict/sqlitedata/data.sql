14652b673c3d2ae7cbfb240095f3675c129d5d2e
podhmo <ababjam61+github@gmail.com>
2019-05-25 03:13:42 +0900

    examples

---
+++ daily/20190525/example_dict/sqlitedata/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO groups VALUES(1, "A");
+INSERT INTO groups VALUES(2, "B");
+INSERT INTO groups VALUES(3, "C");
+
+INSERT INTO users VALUES(10, 1, "Ax");
+INSERT INTO users VALUES(11, 1, "Ay");
+INSERT INTO users VALUES(20, 2, "Bi");
+INSERT INTO users VALUES(40, 4, "D?");
