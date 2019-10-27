1a25f92355d634c14ca30524b9568678904d3669
podhmo <ababjam61+github@gmail.com>
2019-10-28 00:29:15 +0900

    -

--- daily/20191027/example_metashape/11minifs.py
+++ daily/20191027/example_metashape/11minifs.py
@@ -1,12 +1,10 @@
-from io import StringIO
 import logging
-
-from prestring.minifs import MiniFS, Option
+from prestring.python import Module
+from prestring.output import output, cleanup_all  # noqa
 
 logging.basicConfig(level=logging.DEBUG)
 
-option = Option(root="11minifs", cleanup=True, fake=True)
-with MiniFS(default_factory=StringIO, writer=option.writer) as fs:
+with output(root="11minifs") as fs:
     with fs.open("projects/x.txt", "w") as wf:
         print("hello x", file=wf)
         print("bye x", file=wf)
@@ -14,3 +12,7 @@ with MiniFS(default_factory=StringIO, writer=option.writer) as fs:
     with fs.open("projects/y.txt", "w") as wf:
         print("hello y", file=wf)
         print("bye y", file=wf)
+
+    with fs.open("projects/z.py", "w", opener=Module) as m:
+        with m.def_("hello"):
+            m.stmt("print('hello')")
