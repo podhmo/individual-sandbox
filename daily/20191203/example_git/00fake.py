2428cd2dd27cb715627c210da0030392259851eb
podhmo <ababjam61+github@gmail.com>
2019-12-03 18:01:47 +0900

    -

---
+++ daily/20191203/example_git/00fake.py
@@ -0,0 +1,3 @@
+# see: http://softwarelivre.org/terceiro/blog/a-visual-cheat-sheet-for-ansi-color-codes
+print("\x1b[31m-\x1b[7mhello\x1b[0m\x1b[31m world\x1b[0m")
+print("\x1b[32m+\x1b[7mbye-bye\x1b[0m\x1b[32m world\x1b[0m")
