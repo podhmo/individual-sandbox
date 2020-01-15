e095b910095420bc3f6c0fbd3aa703f743d6f68e
podhmo <ababjam61+github@gmail.com>
2020-01-15 17:28:13 +0900

    -

---
+++ daily/20200115/example_grpc/01greeter/greeter_server.py
@@ -0,0 +1,23 @@
+from concurrent import futures
+import grpc
+from handofcats import as_command
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+    def SayHelloAgain(self, request, context):
+        return helloworld_pb2.HelloReply(message="Hello again, %s!" % request.name)
+
+
+@as_command
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
--- daily/20200115/readme.md
+++ daily/20200115/readme.md
@@ -2,6 +2,10 @@
 
 - https://grpc.io/docs/tutorials/basic/python/
 - https://grpc.io/docs/quickstart/python/
+- https://github.com/grpc/grpc/tree/v1.25.0/examples/python/route_guide
+- https://qiita.com/CyLomw/items/9aa4551bd6bb9c0818b6
+
+生成する部分のツールチェインのほうが大切。
 
 ## handofcats loggingを有効にするかどうか
 
