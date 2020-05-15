a0ef3781a2e3217c4e7e9669ea05ae75a4eb946f
podhmo <ababjam61+github@gmail.com>
2020-05-16 07:14:24 +0900

    -

--- daily/20200516/example_egoist/01struct/definitions.py
+++ daily/20200516/example_egoist/01struct/definitions.py
@@ -6,6 +6,7 @@ settings: SettingsDict = {"rootdir": "", "here": __file__}
 app = App(settings)
 
 app.include("egoist.directives.define_struct_set")
+app.include("egoist.directives.define_cli")
 
 
 class Author:
@@ -31,6 +32,32 @@ def model__objects() -> None:
 
     with runtime.generate(structkit, classes=[Article]) as m:
         m.package("model")
+
+
+# cli
+@app.define_cli("egoist.generators.clikit:walk")
+def cmd__load(*, filename: str) -> None:
+    from egoist.generators.clikit import runtime, clikit
+
+    with runtime.generate(clikit) as m:
+        os_pkg = m.import_("os")
+        load_pkg = m.import_("m/load")
+        model_pkg = m.import_("m/model")
+
+        f = m.symbol("f")
+        ob = m.symbol("ob")
+        err = m.symbol("err")
+
+        m.stmt(f"{f}, {err} := {os_pkg.Open(filename)}")
+        m.stmt(f"if {err} != nil {{")
+        with m.scope():
+            m.return_(err)
+        m.stmt("}")
+
+        m.stmt(f"defer {f}.Close()")
+        m.stmt(f"var {ob} {model_pkg.Article}")
+        p = m.let("p", f"&{ob}")
+        m.return_(load_pkg.LoadAndPrint(f, p))
 
 
 if __name__ == "__main__":
---
+++ daily/20200516/example_egoist/01struct/go.mod
@@ -0,0 +1,9 @@
+module m
+
+go 1.14
+
+require (
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/podhmo/maperr v0.2.4
+)
