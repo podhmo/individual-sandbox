0c377c59b9a68e0ba26d7d2f9c1444aef0dee963
podhmo <ababjam61+github@gmail.com>
2020-05-21 00:35:50 +0900

    -

---
+++ daily/20200520/example_egoist/02restapi/apikit/__init__.py
@@ -0,0 +1,18 @@
+class Service:
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def get(self):
+        pass
+
+    def head(self):
+        pass
+
+    def delete(self):
+        pass
