d1d114810417b2851ce8c4393b00ea54a9bcf807
podhmo <ababjam61+github@gmail.com>
2020-09-16 20:39:20 +0900

    web

---
+++ daily/20200916/example_go/cmd/web-server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"m/store"
+	"net/http"
+	"os"
+
+	// defined by us
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+func NewServer() http.Handler {
+	r := chi.NewRouter()
+	s := store.NewTodoStore()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	// r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/api/todos", func(w http.ResponseWriter, r *http.Request) {
+		items := s.List()
+		if items == nil {
+			items = []store.Todo{}
+		}
+		render.JSON(w, r, items)
+	})
+	r.Post("/api/todos", func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var item store.Todo
+		if err := decoder.Decode(&item); err != nil {
+			render.Status(r, 400)
+			render.JSON(w, r, err)
+			return
+		}
+		defer r.Body.Close()
+		s.Add(item)
+		w.WriteHeader(http.StatusCreated)
+	})
+	return r
+}
+
+func run() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":50051"
+	}
+	log.Println("listening ...", port)
+
+	r := NewServer()
+	return http.ListenAndServe(port, r)
+}
--- daily/20200916/example_go/go.mod
+++ daily/20200916/example_go/go.mod
@@ -3,6 +3,8 @@ module m
 go 1.15
 
 require (
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/go-chi/render v1.0.1 // indirect
 	github.com/golang/protobuf v1.4.1
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
