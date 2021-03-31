8f0767e7e7a8932d39e518889bd4a19fcad8a39f
podhmo <ababjam61+github@gmail.com>
2021-03-31 13:21:08 +0900

    -

--- daily/20210331/example_terraform/00hello/main.tf
+++ daily/20210331/example_terraform/00hello/main.tf
@@ -5,11 +5,17 @@ variable "aws_region" {
 locals {
   person = {
     name = "foo"
-    age = 20
+    age  = 20
   }
 }
 
 output "json_result" {
-  value = jsonencode(local.person)
-  greeting = "hello ${local.person.name} from {var.aws_resion}"
+  value    = jsonencode(local.person)
 }
+
+
+output "greeting" {
+  value = "hello ${local.person.name} from ${var.aws_region}"
+}
+
+
--- daily/20210331/example_terraform/02echo-module/main.tf
+++ daily/20210331/example_terraform/02echo-module/main.tf
@@ -1,9 +1,19 @@
-module "plain" {
+module "echo1" {
   source = "./modules/xyz"
 
   primary_configuration = {
     x = 10
     y = 20
     z = 30
+  }
+}
+
+module "echo2" {
+  source = "./modules/xyz"
+
+  primary_configuration = {
+    x = 100
+    y = 200
+    z = 300
   }
 }
