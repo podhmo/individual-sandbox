7044f4716d2f8df62a9cdda0979c1b4a96e96222
podhmo <ababjam61+github@gmail.com>
2023-06-29 14:10:25 +0900

    feat: func

--- daily/20230629/example_go/02custom-func/main_test.go
+++ daily/20230629/example_go/02custom-func/main_test.go
@@ -12,7 +12,8 @@ import (
 type ctxKey string
 
 const (
-	ctxValueKey ctxKey = "value"
+	ctxValueKey  ctxKey = "value"
+	ctxInjectKey ctxKey = "inject"
 )
 
 func Handler(w http.ResponseWriter, req *http.Request) {
@@ -20,15 +21,38 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, `{"value": %q, "ok": %t}`, v, ok)
 }
 
-func TestIt(t *testing.T) {
-	middleware := func(inner http.Handler) http.Handler {
+func InjectComponentsMiddlware(hooks ...func(context.Context) context.Context) func(http.Handler) http.Handler {
+	return func(inner http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			v, _ := req.Context().Value(ctxValueKey).([]string)
-			req = req.WithContext(context.WithValue(req.Context(), ctxValueKey, append(v, "MIDDLEWARE")))
+			ctx := req.Context()
+			for _, inject := range hooks {
+				ctx = inject(ctx)
+			}
+			innerHooks, ok := ctx.Value(ctxInjectKey).([]func(context.Context) context.Context)
+			if ok {
+				for _, inject := range innerHooks {
+					ctx = inject(ctx)
+				}
+			}
+			req = req.WithContext(ctx)
 			inner.ServeHTTP(w, req)
 		})
 	}
+}
+
+func InjectComponentsFunction(req *http.Request, hooks ...func(context.Context) context.Context) *http.Request {
+	ctx := req.Context()
+	innerHooks, _ := ctx.Value(ctxInjectKey).([]func(context.Context) context.Context)
+	return req.WithContext(context.WithValue(ctx, ctxInjectKey, append(innerHooks, hooks...)))
+}
+
+func TestIt(t *testing.T) {
+	middleware := InjectComponentsMiddlware(func(ctx context.Context) context.Context {
+		v, _ := ctx.Value(ctxValueKey).([]string)
+		return context.WithValue(ctx, ctxValueKey, append(v, "MIDDLEWARE"))
+	})
 	h := middleware(http.HandlerFunc(Handler))
+
 	// middleware2 := func(inner http.Handler) http.Handler {
 	// 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 	// 		v, _ := req.Context().Value(ctxValueKey).([]string)
@@ -40,9 +64,12 @@ func TestIt(t *testing.T) {
 
 	rec := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "/", nil)
-	v, _ := req.Context().Value(ctxValueKey).([]string)
-	req = req.WithContext(context.WithValue(req.Context(), ctxValueKey, append(v, "REQUEST")))
+	req = InjectComponentsFunction(req, func(ctx context.Context) context.Context {
+		v, _ := ctx.Value(ctxValueKey).([]string)
+		return context.WithValue(ctx, ctxValueKey, append(v, "REQUEST"))
+	})
 	h.ServeHTTP(rec, req)
+
 	res := rec.Result()
 	if want, got := http.StatusOK, res.StatusCode; want != got {
 		t.Fatalf("unexpected status code: want=%d, but got=%d", want, got)
