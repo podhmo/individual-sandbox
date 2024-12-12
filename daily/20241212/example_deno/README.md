aa1554cd93770047c1dbeff9a5bf5afa2df3254f
podhmo <ababjam61+github@gmail.com>
2024-12-12 21:08:05 +0900

    -

--- daily/20241212/example_deno/README.md
+++ daily/20241212/example_deno/README.md
@@ -9,6 +9,57 @@
 入力したpasswordなどを使ってログインする
 📝 passwordはmaskしたいかも？(defaultでmaskする？)
 
+テキトーに.envに保存することにした.configファイルがほしいかも？
+
+## 02 auth subcommandを作る
+
+そういえばサブコマンドを作る例を書いていたな。
+https://github.com/podhmo/with-help/blob/main/examples/build-subcommands.ts
+
+サブコマンドを書くのはだるいね。。
+
+```ts
+interface BaseOptions {
+    debug: boolean;
+}
+
+async function main() {
+    const baseOptions = parseArgs(Deno.args, {
+        name: "bsky",
+        boolean: ["debug"],
+        stopEarly: true, // for subcommand
+        footer: `
+Available Commands:
+  auth: authentication for bluesky`,
+    });
+
+    const args = baseOptions._;
+    if (args.length === 0) {
+        console.error("%cneed command", "color: red; font-weight: bold");
+        printHelp(baseOptions);
+        return;
+    }
+    switch (args[0]) {
+        case "auth":
+            await authCommand(args.slice(1), baseOptions);
+            break;
+        default:
+            console.error(
+                `%cunknown command: ${args[0]}`,
+                "color: red; font-weight: bold",
+            );
+            printHelp(baseOptions);
+            break;
+    }
+}
+```
+
+## 03 auth statusを作る
+
+auth statusと言いつつauth doctorのようなものかも？
+
+
+
 # references
 
 - https://jsr.io/@std/cli/doc/prompt-secret/~/promptSecret
--- daily/20241212/example_deno/main.ts
+++ daily/20241212/example_deno/main.ts
@@ -1,59 +1,130 @@
 import "jsr:@std/dotenv/load";
-import { parseArgs } from "jsr:@podhmo/with-help@0.5.2";
+import { parseArgs, printHelp } from "jsr:@podhmo/with-help@0.5.2";
 import { promptSecret } from "jsr:@std/cli@1.0.8/prompt-secret";
 import { withTrace } from "jsr:@podhmo/build-fetch@0.1.0";
 
+interface BaseOptions {
+    debug: boolean;
+}
+
 async function main() {
-    // login
-    const args = parseArgs(Deno.args, {
-        string: ["identifier", "password"],
+    const baseOptions = parseArgs(Deno.args, {
+        name: "bsky",
         boolean: ["debug"],
-        required: [],
-        envvar: {
-            identifier: "BSKY_IDENTIFIER",
-            password: "BSKY_PASSWORD",
-        },
+        stopEarly: true, // for subcommand
+        footer: `
+Available Commands:
+  auth: authentication for bluesky`,
     });
 
-    let { identifier, password } = args;
-    console.log("%cneed login", "color: blue; font-weight: bold");
-    if (identifier === undefined) {
-        let input = null;
-        while (input === "" || input === null) {
-            input = promptSecret("  identifier>");
-        }
-        identifier = input.trim();
-    }
-
-    if (password === undefined) {
-        let input = null;
-        while (input === "" || input === null) {
-            input = promptSecret("  password>");
-        }
-        password = input.trim();
-    }
-
-    let fetch = globalThis.fetch;
-    if (args.debug) {
-        fetch = withTrace(fetch);
-    }
-    const res = await Bluesky.login(fetch, { identifier, password });
-    if (!res.ok) {
-        console.error("login failed", res.status, await res.text());
+    const args = baseOptions._;
+    if (args.length === 0) {
+        console.error("%cneed command", "color: red; font-weight: bold");
+        printHelp(baseOptions);
         return;
     }
-
-    console.log("%clogin success", "color: green; font-weight: bold");
-    const session = await res.json();
-    const lines: string[] = [];
-    lines.push("");
-    lines.push(`BSKY_ACCESS_TOKEN = ${session.accessJwt}`);
-    lines.push(`BSKY_REFRESH_TOKEN = ${session.refreshJwt}`);
-
-    if (args.debug) {
-        console.debug("save to .env");
+    switch (args[0]) {
+        case "auth":
+            await authCommand(args.slice(1), baseOptions);
+            break;
+        default:
+            console.error(
+                `%cunknown command: ${args[0]}`,
+                "color: red; font-weight: bold",
+            );
+            printHelp(baseOptions);
+            break;
     }
-    await Deno.writeTextFile(".env", lines.join("\n"), { append: true });
+}
+
+async function authCommand(baseArgs: string[], baseOptions: BaseOptions) {
+    async function login(args: string[], baseOptions: BaseOptions) {
+        // login
+        const options = parseArgs(args, {
+            name: "bsky auth login",
+            string: ["identifier", "password"],
+            boolean: ["debug"],
+            required: [],
+            default: {
+                debug: baseOptions.debug,
+            },
+            envvar: {
+                identifier: "BSKY_IDENTIFIER",
+                password: "BSKY_PASSWORD",
+            },
+        });
+
+        let { identifier, password } = options;
+        console.log("%cneed login", "color: blue; font-weight: bold");
+        if (identifier === undefined) {
+            let input = null;
+            while (input === "" || input === null) {
+                input = promptSecret("  identifier>");
+            }
+            identifier = input.trim();
+        }
+
+        if (password === undefined) {
+            let input = null;
+            while (input === "" || input === null) {
+                input = promptSecret("  password>");
+            }
+            password = input.trim();
+        }
+
+        let fetch = globalThis.fetch;
+        if (options.debug) {
+            fetch = withTrace(fetch);
+        }
+        const res = await Bluesky.login(fetch, { identifier, password });
+        if (!res.ok) {
+            console.error("login failed", res.status, await res.text());
+            return;
+        }
+
+        console.log("%clogin success", "color: green; font-weight: bold");
+        const session = await res.json();
+        const lines: string[] = [];
+        lines.push("");
+        lines.push(`BSKY_ACCESS_TOKEN = ${session.accessJwt}`);
+        lines.push(`BSKY_REFRESH_TOKEN = ${session.refreshJwt}`);
+
+        if (options.debug) {
+            console.debug("save to .env");
+        }
+        await Deno.writeTextFile(".env", lines.join("\n"), { append: true });
+    }
+
+    const options = parseArgs(baseArgs, {
+        name: "bsky auth login",
+        boolean: ["debug"],
+        default: {
+            debug: baseOptions.debug,
+        },
+        stopEarly: true, // for subcommand
+        footer: `
+Available Commands:
+  login: login to bluesky`,
+    });
+
+    const args = options._;
+    if (args.length === 0) {
+        console.error("%cneed command:", "color: red; font-weight: bold");
+        printHelp(options);
+        return;
+    }
+    switch (args[0]) {
+        case "login":
+            await login(args.slice(1), options);
+            break;
+        default:
+            console.error(
+                `%cunknown command: ${args[0]}`,
+                "color: red; font-weight: bold",
+            );
+            printHelp(options);
+            break;
+    }
 }
 
 // deno-lint-ignore no-namespace
