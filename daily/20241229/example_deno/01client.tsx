e5eebb1796f6cc2441c320612d0f1fd5071dabd2
podhmo <ababjam61+github@gmail.com>
2024-12-30 13:40:12 +0900

    -

--- daily/20241229/example_deno/01client.tsx
+++ daily/20241229/example_deno/01client.tsx
@@ -2,21 +2,17 @@
 /** @jsxImportSource npm:react@18 */
 /** @jsxImportSourceTypes npm:@types/react@18 */
 
-import { StrictMode } from "npm:react@18";
+import { StrictMode, useState, useEffect } from "npm:react@18";
 import { createRoot } from "npm:react-dom@18/client";
 
-async function App() {
-    const response = await fetch("/api/readme");
-    if (!response.ok) {
-        return (
-            <section>
-                <h1>hello world</h1>
-                <p>"/readme" {response.statusText}</p>
-            </section>
-        );
-    }
+function App() {
+    const [items, setItems] = useState<string[]>([]);
+    useEffect(() => {
+        fetch("/api/readme")
+            .then((response) => response.json())
+            .then(({ items }) => setItems(items));
+    }, []);
 
-    const { items } = await response.json();
     return (
         <section>
             <h1>hello world</h1>
