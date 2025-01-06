59bb229a400e9d6baa34c8ce0a1c06b478fcb769
podhmo <ababjam61+github@gmail.com>
2025-01-06 23:23:54 +0900

    -

--- daily/20250106/example_deno2/01client.tsx
+++ daily/20250106/example_deno2/01client.tsx
@@ -1,11 +1,11 @@
 /** @jsxRuntime automatic */
-/** @jsxImportSource npm:react@18 */
-/** @jsxImportSourceTypes npm:@types/react@18 */
+/** @jsxImportSource npm:react@19 */
+/** @jsxImportSourceTypes npm:@types/react@19 */
 
-import { StrictMode, useEffect, useState } from "npm:react@18";
-import { createRoot } from "npm:react-dom@18/client";
-/// <ts-types path="npm:@types/react-markdown@8"
-import ReactMarkdown from "npm:react-markdown@8";
+import { StrictMode, useEffect, useState } from "npm:react@19";
+import { createRoot } from "npm:react-dom@19/client";
+/// <ts-types path="npm:@types/react-markdown@9"
+import ReactMarkdown from "npm:react-markdown@9";
 
 const readmeURL =
   "https://gist.githubusercontent.com/podhmo/395689b310af88566f1df31ed218592d/raw/74f9ad1c6897a9d32875338de8734b32383b9aab/README.md";
